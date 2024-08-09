8355ff74d51a02ec4cc227a2a839426bde442aeb
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-09 15:38:03 +0300

    add backends to the main.tf for tracking tfstate

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.114.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "AlfaStorageRG"
+    storage_account_name = "alfawebstorageemhh"
+    container_name       = "alfacontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
